b6a70ecf79736a0c24c5d466dbffdf616dee6713
Eben Hu <EbenHu@outlook.com>
2021-06-16 22:24:36 +0800

    Some annotations.

--- training/buffer.py
+++ training/buffer.py
@@ -91,55 +91,55 @@ class ReplayBuffer(object):
         self.replay_times += 1
         return self.sample(self.replay_batch_size) # here the size of return is [batch_size, 29, dimensions]
 
-class ReplayBufferPro(ReplayBuffer):
-    # this class will samples data while keeping 4 continuity
-        def sample_idxes(self, batch_size):
-            # make sure to keep continuity in future 4 steps
-            idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
-            # firstly keep 4 continuity
-            for ith, idx in enumerate(idxes):
-                temp = self.judge(idx)
-                if temp != idx:
-                    idxes[ith] = temp
-            return np.array(idxes, dtype=np.int32)
-
-        def judge(self, idx):
-            if idx % 200 > 195:
-                new_idx = random.randint(0, len(self._storage) - 1)
-                self.judge(new_idx)
-            else:
-                return idx
-
-        def sample(self, batch_size):
-            idxes = self.sample_idxes(batch_size)
-            return self.sample_with_idxes(idxes)
-
-        def replay(self):
-            if len(self._storage) < self.replay_starts:
-                return None
-            if self.buffer_id == 1 and self.replay_times % self.args.buffer_log_interval == 0:
-                logger.info('Buffer info: {}'.format(self.get_stats()))
-
-            self.replay_times += 1
-            return self.sample(self.replay_batch_size)
-
-        def replay_pro(self, samples):
-            indexs = samples[5]
-            obses_t, actions, rewards, obses_tp1, dones, ref_indexs = [], [], [], [], [], []
-            for idx in indexs:
-                for i in range(4):
-                    # index: [1:4], obses = [s0:s3 + s4], where s4 is the predictive real value
-                    # s0, s1, s2, s3 are 4 historical values, where s3 is the current value
-                    data = self._storage[idx + i]
-                    obs_t, action, reward, obs_tp1, done, ref_index = data
-                    obses_t.append(np.array(obs_t, copy=False))
-                    actions.append(np.array(action, copy=False))
-                    rewards.append(reward)
-                    obses_tp1.append(np.array(obs_tp1, copy=False))
-                    dones.append(done)
-                    ref_indexs.append(ref_index)
-            return np.array(obses_t), np.array(actions), np.array(rewards), \
-                   np.array(obses_tp1), np.array(dones), np.array(ref_indexs)
+# class ReplayBufferPro(ReplayBuffer):
+#     # this class will samples data while keeping 4 continuity
+#         def sample_idxes(self, batch_size):
+#             # make sure to keep continuity in future 4 steps
+#             idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
+#             # firstly keep 4 continuity
+#             for ith, idx in enumerate(idxes):
+#                 temp = self.judge(idx)
+#                 if temp != idx:
+#                     idxes[ith] = temp
+#             return np.array(idxes, dtype=np.int32)
+#
+#         def judge(self, idx):
+#             if idx % 200 > 195:
+#                 new_idx = random.randint(0, len(self._storage) - 1)
+#                 self.judge(new_idx)
+#             else:
+#                 return idx
+#
+#         def sample(self, batch_size):
+#             idxes = self.sample_idxes(batch_size)
+#             return self.sample_with_idxes(idxes)
+#
+#         def replay(self):
+#             if len(self._storage) < self.replay_starts:
+#                 return None
+#             if self.buffer_id == 1 and self.replay_times % self.args.buffer_log_interval == 0:
+#                 logger.info('Buffer info: {}'.format(self.get_stats()))
+#
+#             self.replay_times += 1
+#             return self.sample(self.replay_batch_size)
+#
+#         def replay_pro(self, samples):
+#             indexs = samples[5]
+#             obses_t, actions, rewards, obses_tp1, dones, ref_indexs = [], [], [], [], [], []
+#             for idx in indexs:
+#                 for i in range(4):
+#                     # index: [1:4], obses = [s0:s3 + s4], where s4 is the predictive real value
+#                     # s0, s1, s2, s3 are 4 historical values, where s3 is the current value
+#                     data = self._storage[idx + i]
+#                     obs_t, action, reward, obs_tp1, done, ref_index = data
+#                     obses_t.append(np.array(obs_t, copy=False))
+#                     actions.append(np.array(action, copy=False))
+#                     rewards.append(reward)
+#                     obses_tp1.append(np.array(obs_tp1, copy=False))
+#                     dones.append(done)
+#                     ref_indexs.append(ref_index)
+#             return np.array(obses_t), np.array(actions), np.array(rewards), \
+#                    np.array(obses_tp1), np.array(dones), np.array(ref_indexs)
 
 
 class DistendReplyBuffer(ReplayBuffer):
@@ -162,7 +162,7 @@ class DistendReplyBuffer(ReplayBuffer):
         continuity_judge = True
         current_ref = self._storage[idx][5]
         for i in range(28):
-            if (self._storage[idx-4+i][4] == True) or (self._storage[idx-4+i][5] != current_ref):
+            if self._storage[idx-4+i][4] == True:
                 continuity_judge = False
             return continuity_judge
 
